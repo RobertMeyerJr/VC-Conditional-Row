839de510ff295e0d554bd368125d3f0bde17bc61
Robert Meyer Jr <RobertMeyerJr@gmail.com>
2018-04-19 09:54:00 -0400

    Add backend labels with styling

--- vc-conditional-row.php
+++ vc-conditional-row.php
@@ -14,10 +14,35 @@ class VCConditionalRow{
 	public function __construct(){
 		add_action('init', 					[$this,'init']);
 		add_filter('shortcode_atts_vc_row', [$this, 'visibility']);
+		add_action('admin_enqueue_scripts',	[$this,'admin_css']);			
 	}
+	
+	public function admin_css($hook){
+		if($hook = 'post.php'){
+			?>
+			<style>
+			.vc_admin_label{
+				position:relative;
+				top:-12px;
+			}
+			.vc_admin_label.admin_label_vccr_show_param_name,.vc_admin_label.admin_label_vccr_show_param_value{
+				font-size:14px;				
+			}
+			.admin_label_vccr_show_param_name::before{content:'Conditional Visibility: ';font-weight:bold;}	
+			.admin_label_vccr_show_param_name  label{color:#2ecc71;}
+			.admin_label_vccr_show_param_value label{color:#8e44ad;}
+			
+			.admin_label_vccr_show_param_name:not(.hidden-label) + .admin_label_vccr_show_param_value.hidden-label{display:inline-block !important;}
+			.admin_label_vccr_show_param_value.hidden-label::after{content:'[ Default ]';color:blue;}
+			</style>
+			<?php 
+		}
+	}
+	
 	public function init(){		
 		$settings = array(
 			array(
+				'admin_label' 	=> true,
 				'group' 		=> 'URL Param Visibility',
 				'type' 			=> 'textfield',
 				'heading' 		=> 'Parameter Name',
@@ -25,6 +50,7 @@ class VCConditionalRow{
 				'description' 	=> 'Name of URL parameter to Check',				
 			),
 			array(				
+				'admin_label' 	=> true,
 				'group' 		=> 'URL Param Visibility',
 				'type'			=> 'textfield',
 				'heading'		=> 'Parameter Value',
